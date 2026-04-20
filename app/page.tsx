56ee8ad46a1de441e50835f6dc515f168c97e238
yamarady1-rgb <yamarady1@gmail.com>
2026-04-21 04:25:36 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -213,15 +213,3 @@ export default async function HomePage() {
     </>
   );
 }
-import Image from "next/image";
-
-export default function Page() {
-  return (
-    <Image
-      src="/banner.jpg"
-      alt="Banner"
-      width={500}
-      height={300}
-    />
-  );
-}
